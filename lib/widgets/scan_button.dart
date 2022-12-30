c324595bacd27c4ca23d735fd294234bbf3ec76a
Daniel H <danielhernandez16@hotmail.com>
2022-12-30 11:55:49 -0500

    feat(flutter_barcode_scanner): Install and basic config

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButton extends StatelessWidget {
   const ScanButton({super.key});
@@ -8,8 +7,17 @@ class ScanButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      child: Icon(Icons.filter_center_focus),
-      onPressed: () {}
+      child: const Icon(Icons.filter_center_focus),
+      onPressed: () async {
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#3D8BEF',
+          'Cancelar',
+          false,
+          ScanMode.QR
+        );
+
+        print(barcodeScanRes);
+      }
     );
   }
 }
