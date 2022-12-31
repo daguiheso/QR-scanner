40693a30f343aff2a16d280e9fe0bf935b86f228
Daniel H <danielhernandez16@hotmail.com>
2022-12-30 23:30:05 -0500

    feat(scan_list_provider): Inject global

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import 'package:qr_reader/providers/ui_provider.dart';
+import 'package:qr_reader/providers/scan_list_provider.dart';
 
 import 'package:qr_reader/pages/home_page.dart';
 import 'package:qr_reader/pages/map_page.dart';
@@ -14,7 +16,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => UiProvider())
+        ChangeNotifierProvider(create: (_) => UiProvider()),
+        ChangeNotifierProvider(create: (_) => ScanListProvider())
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
